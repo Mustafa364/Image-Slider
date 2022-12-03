9e4cb5622ffde821b7a5ed00b3e7aa49a64017d0
Ghulam Mustafa <116076196+Mustafa364@users.noreply.github.com>
2022-12-03 16:15:56 +0500

    Add files via upload

---
+++ Slider/app.js
@@ -0,0 +1,18 @@
+
+
+
+var slider_img = document.getElementById('.slider-img');
+var images = ['a.jpg','b.jpg','c.jpg','d.jpg'];
+var i = 0;
+
+function prev(){
+    if(i<= 0 ) i = images.length; i--;
+    return setImg();
+}
+function next(){
+    if(i >= images.length-1) i = -1; i++;
+    return setImg();
+}
+function setImg(){
+    return slider_img.setAttribute('src', 'images/'+images[i]);
+}
